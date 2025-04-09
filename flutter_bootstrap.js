f4300dbe6e3582a8559c767bb51fc067d50dbf36
fe5tus001 <festuskaoma@gmail.com>
2025-04-10 01:07:47 +0200

    10-April

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1773923589"
+    serviceWorkerVersion: "3777358416"
   }
 });
